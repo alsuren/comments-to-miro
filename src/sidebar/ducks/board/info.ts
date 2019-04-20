4005e5784e8fb5c1e156adbd76843d2025feb3b3
David Laban <alsuren@gmail.com>
2019-04-20 17:17:26 +0100

    add redux ducks, following https://redux.js.org/recipes/usage-with-typescript

---
+++ src/sidebar/ducks/board/info.ts
@@ -0,0 +1,86 @@
+import { AnyARecord } from "dns";
+
+const defaultState = {
+    loading: false,
+    err: null,
+    info: null,
+};
+
+// Action Types
+const GET_INFO_REQUEST = 'info/GET_INFO_REQUEST';
+const GET_INFO_FAILURE = 'info/GET_INFO_FAILURE';
+const GET_INFO_SUCCESS = 'info/GET_INFO_SUCCESS';
+
+interface GetInfoRequestAction {
+    readonly type: typeof GET_INFO_REQUEST;
+}
+interface GetInfoFailureAction {
+    readonly type: typeof GET_INFO_FAILURE;
+    readonly err: {};
+}
+interface GetInfoSuccessAction {
+    readonly type: typeof GET_INFO_SUCCESS;
+    readonly info: {};
+}
+type Action = GetInfoRequestAction | GetInfoFailureAction | GetInfoSuccessAction;
+// Reducer
+export default function reducer(state = defaultState, action: Action) {
+    switch (action.type) {
+        case GET_INFO_REQUEST: {
+            return {
+                ...state,
+                loading: true,
+                err: null,
+            };
+        }
+        case GET_INFO_FAILURE: {
+            return {
+                ...state,
+                loading: false,
+                err: action.err,
+            };
+        }
+        case GET_INFO_SUCCESS: {
+            return {
+                ...state,
+                loading: false,
+                info: action.info,
+            };
+        }
+
+        default: {
+            return state;
+        }
+    }
+}
+
+// Action Creators
+
+export function loadInfoRequest() {
+    return { type: GET_INFO_REQUEST };
+}
+
+export function loadInfoFailure(err) {
+    return {
+        type: GET_INFO_FAILURE,
+        err,
+    };
+}
+
+export function loadInfoSuccess(info) {
+    return {
+        type: GET_INFO_SUCCESS,
+        info,
+    };
+}
+
+// Thunks
+
+export const loadInfo = () => async dispatch =>  {
+    try {
+        let info = await rtb.board.info.get();
+    } catch (err) {
+        dispatch(loadInfoFailure(err))
+    }
+    dispatch(loadInfoSuccess);
+}
